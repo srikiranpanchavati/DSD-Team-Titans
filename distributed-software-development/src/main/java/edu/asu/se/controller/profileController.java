a71ebe341ec39f70c01931b8a124e0569bb464ea
Kiran <srikiran.phoenix@gmail.com>
2015-10-16 01:17:04 -0700

    Updated view and controller for profile page.

---
+++ distributed-software-development/src/main/java/edu/asu/se/controller/profileController.java
@@ -0,0 +1,20 @@
+package edu.asu.se.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class profileController {
+	@RequestMapping(value = "/profile**", method = RequestMethod.GET)
+	public ModelAndView profile() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Profile Page");
+		model.addObject("message", "Welcome Home");
+		model.setViewName("profile");
+
+		return model;
+	}
+}
