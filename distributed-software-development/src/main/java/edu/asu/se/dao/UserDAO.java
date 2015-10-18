3e74ef18ce5f0920a68625dc8083759d80692ae7
yogaviswani <viswani.yadalam@gmail.com>
2015-10-17 22:55:46 -0700

    DAO file

--- distributed-software-development/src/main/java/edu/asu/se/dao/UserDAO.java
+++ distributed-software-development/src/main/java/edu/asu/se/dao/UserDAO.java
@@ -1,10 +1,15 @@
 package edu.asu.se.dao;
 
+import java.util.List;
+
 import edu.asu.se.model.GitProjectDetails;
 import edu.asu.se.model.User;
 
 public interface UserDAO {
 
 	public User findUserByName(String userName);
-	public GitProjectDetails getProjectDetails(String userName);
+
+	public List<GitProjectDetails> getProjectDetails(String userName);
+
+	public void insertProjectDetails(GitProjectDetails gitProjectDetails);
 }
