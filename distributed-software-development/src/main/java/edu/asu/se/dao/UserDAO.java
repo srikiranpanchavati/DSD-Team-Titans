f29f226110497e6068a726108f82023958646cf9
srikiranpanchavati <srikiran.phoenix@gmail.com>
2015-11-04 16:30:05 -0700

    updated userDAO for retrieving file stats.

--- distributed-software-development/src/main/java/edu/asu/se/dao/UserDAO.java
+++ distributed-software-development/src/main/java/edu/asu/se/dao/UserDAO.java
@@ -2,16 +2,25 @@ package edu.asu.se.dao;
 
 import java.util.List;
 
+import edu.asu.se.model.CodeStatistics;
+import edu.asu.se.model.FileActivityDetails;
 import edu.asu.se.model.GitProjectDetails;
 import edu.asu.se.model.User;
+import edu.asu.se.model.UserActivityDetails;
 
 public interface UserDAO {
+	
+	public boolean insertUserRegisterationDetails(User user);
 
 	public User findUserByName(String userName);
 
 	public List<GitProjectDetails> getProjectDetails(String userName);
 
 	public void insertProjectDetails(String userName, GitProjectDetails gitProjectDetails);
-	
-	public GitProjectDetails getStats(GitProjectDetails details);
+
+	public List<FileActivityDetails> getFileActivityDetails(GitProjectDetails details);
+
+	public List<UserActivityDetails> getUserActivityDetails(GitProjectDetails details);
+
+	public List<CodeStatistics> getCodeStatistics(GitProjectDetails details);
 }
--- distributed-software-development/src/main/java/edu/asu/se/dao/mongodb/impl/UserDAOMongoImpl.java
+++ distributed-software-development/src/main/java/edu/asu/se/dao/mongodb/impl/UserDAOMongoImpl.java
@@ -1,18 +1,22 @@
 package edu.asu.se.dao.mongodb.impl;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 
 import edu.asu.se.dao.UserDAO;
+import edu.asu.se.model.CodeStatistics;
+import edu.asu.se.model.FileActivityDetails;
 import edu.asu.se.model.GitProjectDetails;
 import edu.asu.se.model.User;
+import edu.asu.se.model.UserActivityDetails;
 
 @Repository
 public class UserDAOMongoImpl implements UserDAO {
@@ -22,6 +26,20 @@ public class UserDAOMongoImpl implements UserDAO {
 
 	private static final String USER_COLLECTION = "users";
 
+	@Override
+	public boolean insertUserRegisterationDetails(User user)
+	{
+		user.setEnabled(true);
+		user.setRole("ROLE_USER");
+		User availableUser = findUserByName(user.getUsername());
+		if (availableUser != null)
+		{
+			return false;
+		}
+		mongoTemplate.insert(user, USER_COLLECTION);
+		return true;
+	}
+	
 	@Override
 	public User findUserByName(String userName) {
 		Query query = new Query(Criteria.where("username").is(userName));
@@ -54,15 +72,44 @@ public class UserDAOMongoImpl implements UserDAO {
 		}
 	}
 
-	@Override
-	public GitProjectDetails getStats(GitProjectDetails details) {
-		// Query query2 = new
-		// Query(Criteria.where("branchDetails.branchName").is(details.getBranchDetails().get(0).getBranchName()));
-		Query query = new Query(
-				Criteria.where("branchDetails.branchName").is(details.getBranchDetails().get(0).getBranchName())).addCriteria(
-				Criteria.where("_id").is(details.getId()).and("branchDetails").elemMatch(
-								Criteria.where("branchName").is(details.getBranchDetails().get(0).getBranchName())));
-		return mongoTemplate.findOne(query, GitProjectDetails.class, "projectDetails");
+	private GitProjectDetails getBranchDetails(GitProjectDetails details) {
+		BasicDBObject idCriteria = new BasicDBObject();
+		idCriteria.put("_id", details.getId());
+		BasicDBObject branchNameCriteria = new BasicDBObject();
+		branchNameCriteria.put("branchName", details.getBranchDetails().get(0).getBranchName());
+		BasicDBObject eleMatch = new BasicDBObject();
+		eleMatch.put("$elemMatch", branchNameCriteria);
+		idCriteria.append("branchDetails", eleMatch);
+
+		BasicDBObject query = new BasicDBObject();
+		query.put("branchDetails.branchName", details.getBranchDetails().get(0).getBranchName());
+
+		DBObject result = mongoTemplate.getCollection("projectDetails").findOne(query, idCriteria);
+		return mongoTemplate.getConverter().read(GitProjectDetails.class, result);
+
 	}
 
+	@Override
+	public List<FileActivityDetails> getFileActivityDetails(GitProjectDetails details) {
+		GitProjectDetails branchDetails = getBranchDetails(details);
+		Query query = new Query(
+				Criteria.where("_id").in(branchDetails.getBranchDetails().get(0).getFileActivityDetailsId()));
+		return mongoTemplate.find(query, FileActivityDetails.class, "FileActivityDetails");
+	}
+
+	@Override
+	public List<UserActivityDetails> getUserActivityDetails(GitProjectDetails details) {
+		GitProjectDetails branchDetails = getBranchDetails(details);
+		Query query = new Query(
+				Criteria.where("_id").in(branchDetails.getBranchDetails().get(0).getUserActivityDetailsId()));
+		return mongoTemplate.find(query, UserActivityDetails.class, "UserActivityDetails");
+	}
+
+	@Override
+	public List<CodeStatistics> getCodeStatistics(GitProjectDetails details) {
+		GitProjectDetails branchDetails = getBranchDetails(details);
+		Query query = new Query(
+				Criteria.where("_id").in(branchDetails.getBranchDetails().get(0).getCodeStatisticsId()));
+		return mongoTemplate.find(query, CodeStatistics.class, "CodeStatistics");
+	}
 }
