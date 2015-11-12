26e4ff61746a3e825eac4d99f36eb29273c040f8
Supraj Kolluri <supcena@gmail.com>
2015-11-12 00:04:34 -0700

    Adding code stats dao implementation

---
+++ distributed-software-development/src/main/java/edu/asu/se/dao/mongodb/impl/CodeStatsDAOMongoImpl.java
@@ -0,0 +1,33 @@
+package edu.asu.se.dao.mongodb.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import edu.asu.se.dao.CodeStatsDAO;
+import edu.asu.se.dao.ProjectDetailsDAO;
+import edu.asu.se.model.CodeStatistics;
+import edu.asu.se.model.GitProjectDetails;
+
+public class CodeStatsDAOMongoImpl implements CodeStatsDAO {
+
+	@Autowired
+	MongoTemplate mongoTemplate;
+
+	@Autowired
+	ProjectDetailsDAO projectDetailsDAO;
+
+	private static final String CODESTATISTICS_COLLECTION = "CodeStatistics";
+
+	@Override
+	public List<CodeStatistics> getCodeStatistics(GitProjectDetails details) {
+		GitProjectDetails branchDetails = projectDetailsDAO.getBranchDetails(details);
+		Query query = new Query(
+				Criteria.where("_id").in(branchDetails.getBranchDetails().get(0).getCodeStatisticsId()));
+		return mongoTemplate.find(query, CodeStatistics.class, CODESTATISTICS_COLLECTION);
+	}
+
+}
