8b0c60a5013f19711bdcc0dbadbcef13fc028fd0
Supraj Kolluri <supcena@gmail.com>
2015-11-15 20:23:56 -0700

    updating project details dao

--- distributed-software-development/src/main/java/edu/asu/se/model/GitProjectDetails.java
+++ distributed-software-development/src/main/java/edu/asu/se/model/GitProjectDetails.java
@@ -10,6 +10,7 @@ public class GitProjectDetails {
 	private String id;
 	private String projectName;
 	private String projectUrl;
+	private String rootPOMLoc;
 	private List<BranchDetails> branchDetails;
 
 	public String getId() {
@@ -42,6 +43,14 @@ public class GitProjectDetails {
 
 	public void setProjectUrl(String projectUrl) {
 		this.projectUrl = projectUrl;
+	}
+
+	public String getRootPOMLoc() {
+		return rootPOMLoc;
+	}
+
+	public void setRootPOMLoc(String rootPOMLoc) {
+		this.rootPOMLoc = rootPOMLoc;
 	}
 
 }
