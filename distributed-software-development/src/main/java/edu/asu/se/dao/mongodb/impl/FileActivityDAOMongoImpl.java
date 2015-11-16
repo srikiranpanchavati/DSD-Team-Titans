8295c7c617f993576f1c2f7e4543666ace5b926e
Supraj Kolluri <supcena@gmail.com>
2015-11-15 19:45:11 -0700

    updating file activity dao file

--- distributed-software-development/src/main/java/edu/asu/se/dao/mongodb/impl/FileActivityDAOMongoImpl.java
+++ distributed-software-development/src/main/java/edu/asu/se/dao/mongodb/impl/FileActivityDAOMongoImpl.java
@@ -31,16 +31,21 @@ public class FileActivityDAOMongoImpl implements FileActivityDAO {
 	@Override
 	public List<FileActivityDetails> getFileActivityDetails(GitProjectDetails details) {
 		GitProjectDetails branchDetails = projectDetailsDAO.getBranchDetails(details);
-		Query query = new Query(
-				Criteria.where("_id").in(branchDetails.getBranchDetails().get(0).getFileActivityDetailsId()));
-		return mongoTemplate.find(query, FileActivityDetails.class, FILEACTIVITY_COLLECTION);
+		List<String> fileIds = branchDetails.getBranchDetails().get(0).getFileActivityDetailsId();
+		if (fileIds != null) {
+			Query query = new Query(Criteria.where("_id").in(fileIds).and("projName").is(details.getProjectName())
+					.and("branchName").is(details.getBranchDetails().get(0).getBranchName()));
+			return mongoTemplate.find(query, FileActivityDetails.class, FILEACTIVITY_COLLECTION);
+		} else
+			return new ArrayList<FileActivityDetails>();
 	}
 
 	@Override
 	public void insertFileActivityDetails(GitProjectDetails projDetails, FileActivityDetails fileDetail) {
 		String fileId = "";
 		Query query = new Query(
-				Criteria.where("fileName").is(fileDetail.getFileName()).and("filePath").is(fileDetail.getFilePath()));
+				Criteria.where("fileName").is(fileDetail.getFileName()).and("filePath").is(fileDetail.getFilePath())
+						.and("projName").is(fileDetail.getProjName()).and("branchName").is(fileDetail.getBranchName()));
 		FileActivityDetails availableFile = mongoTemplate.findOne(query, FileActivityDetails.class,
 				"FileActivityDetails");
 		if (availableFile != null) {
@@ -55,10 +60,11 @@ public class FileActivityDAOMongoImpl implements FileActivityDAO {
 		String branchName = projDetails.getBranchDetails().get(0).getBranchName();
 		projDetails = mongoTemplate.findOne(query, GitProjectDetails.class, "projectDetails");
 		for (BranchDetails branchDetail : projDetails.getBranchDetails()) {
-			if (branchDetail.getBranchName() == branchName) {
+			if (branchDetail.getBranchName().equals(branchName)) {
 				if (branchDetail.getFileActivityDetailsId() == null) {
 					List<String> fileIds = new ArrayList<String>();
 					fileIds.add(fileId);
+					branchDetail.setFileActivityDetailsId(fileIds);
 				} else if (!branchDetail.getFileActivityDetailsId().contains(fileId)) {
 					branchDetail.getFileActivityDetailsId().add(fileId);
 				}
