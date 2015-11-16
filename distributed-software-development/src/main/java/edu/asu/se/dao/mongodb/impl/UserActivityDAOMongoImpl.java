a79146fb0d9879e635f8f4107f32caceab9a19ef
rathnakar14 <rathnakar.kallamadi@gmail.com>
2015-11-16 01:49:07 -0700

    User Activity DAO update

--- distributed-software-development/src/main/java/edu/asu/se/dao/mongodb/impl/UserActivityDAOMongoImpl.java
+++ distributed-software-development/src/main/java/edu/asu/se/dao/mongodb/impl/UserActivityDAOMongoImpl.java
@@ -31,37 +31,45 @@ public class UserActivityDAOMongoImpl implements UserActivityDAO {
 	@Override
 	public List<UserActivityDetails> getUserActivityDetails(GitProjectDetails details) {
 		GitProjectDetails branchDetails = projectDetailsDAO.getBranchDetails(details);
-		Query query = new Query(
-				Criteria.where("_id").in(branchDetails.getBranchDetails().get(0).getUserActivityDetailsId()));
-		return mongoTemplate.find(query, UserActivityDetails.class, "UserActivityDetails");
+		List<String> userIds = branchDetails.getBranchDetails().get(0).getUserActivityDetailsId();
+		if (userIds != null) {
+			Query query = new Query(Criteria.where("userName").in(userIds).and("projName").is(details.getProjectName())
+					.and("branchName").is(details.getBranchDetails().get(0).getBranchName()));
+			return mongoTemplate.find(query, UserActivityDetails.class, USERACTIVITY_COLLECTION);
+		} else {
+			return new ArrayList<UserActivityDetails>();
+		}
 	}
 
 	@Override
 	public void insertUserActivityDetails(GitProjectDetails projDetails, UserActivityDetails userDetail) {
-		Query query = new Query(Criteria.where("userName").is(userDetail.getUserName()));
+		Query query = new Query(Criteria.where("userName").is(userDetail.getUserName()).and("projName")
+				.is(userDetail.getProjName()).and("branchName").is(userDetail.getBranchName()));
 		UserActivityDetails availableUser = mongoTemplate.findOne(query, UserActivityDetails.class,
-				"UserActivityDetails");
+				USERACTIVITY_COLLECTION);
 		if (availableUser != null) {
 			userDetail.setId(availableUser.getId());
 		}
 
 		mongoTemplate.save(userDetail, USERACTIVITY_COLLECTION);
 
-		String userId = userDetail.getId();
+		String userId = userDetail.getUserName();
 
 		query = new Query(Criteria.where("projectName").is(projDetails.getProjectName()));
 		String branchName = projDetails.getBranchDetails().get(0).getBranchName();
 		projDetails = mongoTemplate.findOne(query, GitProjectDetails.class, PROJECTDETAILS_COLLECTION);
 		for (BranchDetails branchDetail : projDetails.getBranchDetails()) {
-			if (branchDetail.getBranchName() == branchName) {
+			if (branchDetail.getBranchName().equals(branchName)) {
 				if (branchDetail.getUserActivityDetailsId() == null) {
-					List<String> userIds = new ArrayList<String>();
-					userIds.add(userId);
+					List<String> usernames = new ArrayList<String>();
+					usernames.add(userId);
+					branchDetail.setUserActivityDetailsId(usernames);
 				} else if (!branchDetail.getUserActivityDetailsId().contains(userId)) {
 					branchDetail.getUserActivityDetailsId().add(userId);
 				}
 			}
 		}
+		mongoTemplate.save(projDetails, PROJECTDETAILS_COLLECTION);
 	}
 
 }
